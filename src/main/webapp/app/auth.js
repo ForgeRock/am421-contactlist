1f46cb4ef628ab2320b7162b1ade989f79699ecc
Peter Varga (VM) <peter.varga@dpc.hu>
2018-11-25 09:47:36 -0500

    TODO comments

--- src/main/webapp/app/auth.js
+++ src/main/webapp/app/auth.js
@@ -3,7 +3,7 @@ angular
 //DONE Ch4L1Ex3: modify dependency to services.auth.openam-min
 //DONE Ch4L2Ex1: modify dependency to services.auth.openam
 //DONE Ch5L1Ex3: modify dependency to services.auth.oauth2
-//TODO lab12: modify dependency to services.auth.uma
-                ['services.auth.oauth2']); //selects the implementation of loginService
+//TODO Ch6L1Ex2: modify dependency to services.auth.uma
+                ['services.auth.openam']); //selects the implementation of loginService
 
 
